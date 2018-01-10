f0ce8c491ce3f8c905b72951293f230df72430ba
Stephan Diehl <stephan@transvection.de>
2018-01-10 22:24:47 +0100

    Python 3

--- python/spiel.py
+++ python/spiel.py
@@ -17,16 +17,16 @@ def start_spielfeld():
 
 def zeichne_spielfeld_reihe(reihe, spielfeld):
     unterer_index = reihe*8
-    zahlen = ["{:^4}".format(spielfeld[i]) for i in xrange(unterer_index, unterer_index+8)]
+    zahlen = ["{:^4}".format(spielfeld[i]) for i in range(unterer_index, unterer_index+8)]
     if reihe in (1, 2):
         zahlen.reverse()
-    print "|" + "|".join(zahlen) + "|"
+    print( "|" + "|".join(zahlen) + "|")
 
 def zeichne_spielfeld(spielfeld):
-    print "-"*41
-    for reihe in xrange(3, -1, -1):
+    print( "-"*41)
+    for reihe in range(3, -1, -1):
         zeichne_spielfeld_reihe(reihe, spielfeld)
-        print "-"*41
+        print("-"*41)
 
 def gueltig(spieler, position, spielfeld):
     return position.isdigit() and int(position) < 16 and spielfeld[index(spieler, int(position))] > 1
@@ -39,7 +39,7 @@ def zug(spieler, position, spielfeld, take_aside=True):
         raise ValueError("Wir brauchen mehr als 2 Steine")
     spielfeld[spielfeld_index] = 0
 
-    for i in xrange(steine):
+    for i in range(steine):
         spielfeld_index = index(spieler, (position + i + 1) % 16)
         spielfeld[spielfeld_index] += 1
 
@@ -68,20 +68,20 @@ def spiel():
     zeichne_spielfeld(spielfeld)
     while 1:
         spieler = spielzug % 2
-        input = "U:" if spieler == 0 else "O:"
-        position = raw_input(input)
+        input_str = "U:" if spieler == 0 else "O:"
+        position = input(input_str)
         if position == 'q':
             sys.exit()
         while not gueltig(spieler, position, spielfeld):
-            print "Bitte Zahl zwischen 0 und 15 eingeben! (Mindestens 2 Steine auf Feld)"
-            position = raw_input(input)
+            print("Bitte Zahl zwischen 0 und 15 eingeben! (Mindestens 2 Steine auf Feld)")
+            position = input(input_str)
         spielfeld = zug(spieler, int(position), spielfeld, spielzug > 1)
         zeichne_spielfeld(spielfeld)
         if max(spielfeld[:15]) < 2:
-            print "Spieler O hat gewonnen"
+            print("Spieler O hat gewonnen")
             sys.exit()
         elif max(spielfeld[16:]) < 2:
-            print "Spieler U hat gewonnen"
+            print("Spieler U hat gewonnen")
             sys.exit()
         spielzug += 1
 
